74a15507cfc6bfd42f0144c2373192f1d5476c37
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-08-22 23:06:14 +0300

    feat(hiring): expose LinkedIn candidate sources

--- server/routes/internal/hiring-channels.get.ts
+++ server/routes/internal/hiring-channels.get.ts
@@ -9,6 +9,7 @@ import { hiringIshBorSourceHandles } from '~~/server/utils/hiringIshBorSource'
 import { hiringSecondaryWebSourceHandles } from '~~/server/utils/hiringSecondaryWebSources'
 import { hiringUzJobsSourceHandles } from '~~/server/utils/hiringUzJobsSource'
 import { hiringSocialSourceHandles } from '~~/server/utils/hiringSocialSources'
+import { hiringLinkedInSourceHandles } from '~~/server/utils/hiringLinkedInSources'
 import { loadCursors, loadWebCursors } from '~~/server/utils/hiringCursors'
 
 export default defineEventHandler(async (event) => {
@@ -40,6 +41,7 @@ export default defineEventHandler(async (event) => {
     ...progressiveWebHandles,
     ...hiringSecondaryWebSourceHandles(),
     ...hiringSocialSourceHandles(),
+    ...hiringLinkedInSourceHandles(),
   ]
   const [telegramCursors, webCursors] = await Promise.all([loadCursors(), loadWebCursors()])
   const backfillHandles = [
