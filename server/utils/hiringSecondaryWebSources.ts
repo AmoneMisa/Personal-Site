00dca131b2f2f58a2b09f7e3a2886287eddde88c
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-08-24 01:43:03 +0300

    refactor(hiring): remove unused compatibility facades

--- server/utils/hiringSecondaryWebSources.ts
+++
@@ -1,5 +0,0 @@
-export {
-  crawlSecondaryWebSource,
-  refreshHiringSecondaryWebSource,
-} from '../hiring/sources/secondaryWebRefresh'
-export { hiringSecondaryWebSourceHandles } from '../../shared/hiring/sources/secondaryWebSources'
