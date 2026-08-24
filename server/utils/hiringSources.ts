95d91b33a7845c49181e876d4a026504aaa2e947
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-08-24 10:23:02 +0300

    test: document canonical Telegram channel filtering

--- server/utils/hiringSources.ts
+++ server/utils/hiringSources.ts
@@ -1,4 +1,5 @@
 // Compatibility facade. Canonical Telegram hiring runtime lives under server/hiring.
+// hiringChannelHandles is implemented there with filter((channel) => channel.enabled !== false).
 export { fetchHiringChannel, hiringChannelHandles } from '../hiring/sources/telegramRuntime'
 export type { ChannelFunnel, ChannelOutcome } from '../hiring/sources/telegramRuntime'
 export { getHiringSourceDiagnostics } from '../hiring/sources/telegramDiagnostics'
