bcc085d629ecefa2c7a34151b562fa00c52fbae9
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-08-25 11:46:34 +0300

    refactor: use shared Tashkent metro lexicon

--- server/utils/tashkentMetroLabels.ts
+++ server/utils/tashkentMetroLabels.ts
@@ -1,30 +1,14 @@
-const RU: Record<string, string> = {
-  'Buyuk Ipak Yoli': 'Буюк Ипак Йули', Pushkin: 'Пушкин', 'Hamid Olimjon': 'Хамид Алимджан',
-  'Amir Temur Xiyoboni': 'Амир Темур Хиёбони', 'Mustaqillik Maydoni': 'Мустакиллик майдони', Paxtakor: 'Пахтакор',
-  'Xalqlar Dostligi': 'Халклар Дустлиги', 'Milliy Bog': 'Миллий Бог', Novza: 'Новза', 'Mirzo Ulugbek': 'Мирзо Улугбек',
-  Chilonzor: 'Чиланзар', Olmazor: 'Алмазар', Choshtepa: 'Чаштепа', Ozgarish: 'Узгариш', Sergeli: 'Сергели', Yangihayot: 'Янгихаёт', Chinor: 'Чинар',
-  Beruniy: 'Беруни', Tinchlik: 'Тинчлик', Chorsu: 'Чорсу', 'Gafur Gulom': 'Гафур Гулям', 'Alisher Navoi': 'Алишер Навои',
-  Ozbekiston: 'Узбекистан', Kosmonavtlar: 'Космонавтлар', Oybek: 'Ойбек', Toshkent: 'Ташкент', Mashinasozlar: 'Машинасозлар', Dostlik: 'Дустлик',
-  Turkiston: 'Туркистон', Yunusobod: 'Юнусабад', Shahriston: 'Шахристан', Bodomzor: 'Бадамзар', Minor: 'Минор',
-  'Abdulla Qodiriy': 'Абдулла Кадыри', 'Yunus Rajabiy': 'Юнус Раджаби', 'Ming Orik': 'Мингурик',
-  Texnopark: 'Технопарк', Yashnobod: 'Яшнабад', Tuzel: 'Тузель', Olmos: 'Алмас', Rohat: 'Рохат', Yangiobod: 'Янгиабад', Qoyliq: 'Куйлюк',
-  Matonat: 'Матонат', Qiyot: 'Кият', Tolariq: 'Толарык', Xonobod: 'Хонабад', Quruvchilar: 'Курувчилар', Turon: 'Туран', Qipchoq: 'Кипчак',
-}
-
-const LEGACY_TO_CANONICAL: Record<string, string> = {
-  'Buyuk Ipak Yuli': 'Buyuk Ipak Yoli', 'Буюк Ипак Йули': 'Buyuk Ipak Yoli',
-  Bunyodkor: 'Xalqlar Dostligi', 'Бунёдкор': 'Xalqlar Dostligi', 'Халклар Дустлиги': 'Xalqlar Dostligi',
-  'Qo‘yliq': 'Qoyliq', "Qo'yliq": 'Qoyliq', 'Куйлюк': 'Qoyliq',
-  'Мирзо Улугбек': 'Mirzo Ulugbek', 'Чиланзар': 'Chilonzor', 'Юнусабад': 'Yunusobod',
-}
-
-const RU_TO_CANONICAL = Object.fromEntries(Object.entries(RU).map(([canonical, ru]) => [ru, canonical]))
+import {
+  TASHKENT_METRO_BY_NAME,
+  canonicalTashkentMetro,
+} from '@whiteslove/parsing-lexicon/geo'
 
 export function metroLabel(value: string, locale = 'en'): string {
-  const canonical = LEGACY_TO_CANONICAL[value] || value
-  return locale.toLowerCase().startsWith('ru') ? (RU[canonical] || value) : canonical
+  const canonical = canonicalTashkentMetro(value) || value
+  if (!locale.toLowerCase().startsWith('ru')) return canonical
+  return TASHKENT_METRO_BY_NAME.get(canonical)?.labels.ru || value
 }
 
 export function canonicalMetroValue(value: string): string {
-  return RU_TO_CANONICAL[value] || LEGACY_TO_CANONICAL[value] || value
+  return canonicalTashkentMetro(value) || value
 }
